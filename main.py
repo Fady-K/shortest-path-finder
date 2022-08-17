23b3d7cf288cf8852cec93a1ef9a8c37db88d795
Fady Kamal <93720162+Fady-K@users.noreply.github.com>
2022-08-17 13:54:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import queue
-import time
 import curses
 from curses import wrapper
+import queue
+import time
 
 maze = [
     ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
@@ -16,120 +16,122 @@ maze = [
 ]
 
 
-def print_maze(maze, stdscr, path):
+
+def print_maze(maze, stdscr, path=[]):
     blue = curses.color_pair(1)
     red = curses.color_pair(2)
-
-    # enumerate take iterabel and make it a counter 
-    for i, row in enumerate(maze):      # iterate over indexes of rows and rows of the maze
-        for j, value in enumerate(row): # iterate over indexes of columns inside each row
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
             if (i, j) in path:
-                stdscr.addstr(i, j * 2, "S", red)
+                stdscr.addstr(i, j*2, "X", red)
             else:
-                stdscr.addstr(i, j * 2, value, blue)
+                stdscr.addstr(i, j*2, value, blue)
 
 
 
-# algorithm 
-# find the start node position
-def find_start(maze, start):        # iterate over each value of the maze if any equalls the start node returns it's position
+def find_start(maze, start):
     for i, row in enumerate(maze):
         for j, value in enumerate(row):
             if value == start:
-                return i, j       #
-
-    else:
-        return None
+                return i, j
+                break
 
 
-def find_all_neighbors(maze, row, col):
-    neighbors = []
-
-    # get the upper neighbor =>>>> only if the index of the current row is bigger than the 0
-    if row > 0:
-        neighbors.append((row - 1, col))        # UP
-    
-    # get the beneth neighbor (downward one) =>>>>> only if the current row is smaller than the last row index
-    if row + 1 < len(maze):
-        neighbors.append((row + 1, col))        # DOWN
-    
-    # get the left neighbor =>>>>>>> only if the current column is bigger than 0
-    if col > 0:
-        neighbors.append((row, col - 1))        # LEFT
-    
-    # get the right neighbore =>>>>>> only if the col + 1 smaller than len(maze[0]) >> currenct row
-    if col + 1 < len(maze[0]):
-        neighbors.append((row, col + 1))        # RIGHT
-
-    return neighbors
+# This function returns the end point position
+def find_end(maze):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == "X":
+                return i, j
+                break
+ 
 
 
-def find_shortest_path(maze, stdscr):
-    # declare and define the start node position
+def find_path(maze, stdscr):
     start = "O"
     end = "X"
-
-    # breadth first searsh
     start_pos = find_start(maze, start)
-
-    # declare the queue and add the first node pos and start a path starting with start pos as the first
+    
+    # initialize the queue
     q = queue.Queue()
-
     q.put(((start_pos), [start_pos]))
 
-    # declare visited set to collect all visited node so as not to be checked again
-    visited = set()         # set is a data structure that removes dublicate numbers
+    # don't check nodes in visisted set
+    visited = set()
 
 
-    # init the (BFS)     Algorithm
-    while not q.empty():        # while queue not empty
-        
-        # get latest pair in the queue >>>>>> pair (currenct_pos, [path])
-        current_pos, path = q.get()
+    # breadth first search alogrithm
+    while not q.empty():
+        currenct_pos, path = q.get()
+        row, col = currenct_pos
 
-        # print the maze with unique path
+
+        # print the maze every new iteration with red path
         stdscr.clear()
         print_maze(maze, stdscr, path)
         stdscr.refresh()
         time.sleep(0.2)
-        # break currect_pos pair into row and column (cartasian coordinates)
-        row, col = current_pos
 
-        # if the coordination belongs to the end, return the path
+        # check if node whose pair == (row, col) is end
         if maze[row][col] == end:
             return path
-        
-        
 
-        # check all neighbors
+        # get all neighbors to check them 
+        neighbors = find_neighbors(maze, row, col)
 
-        neighbors = find_all_neighbors(maze, row, col)
 
+        # check the fetched neighbor
         for neighbor in neighbors:
-            r, c = neighbor
 
-
-            # continue if neigbor is already checked (in visited)
+            # check if neighbore is already visited
             if neighbor in visited:
                 continue
 
-            # coninue if neighbor is an obstacle
+            # if neighbor is obstacle ignore it (continue)
+            r, c = neighbor
             if maze[r][c] == "#":
                 continue
 
-        
-            # put this neighbor into the queue and add new path comprisd of old path + new node (neighbor)
+            # form the new path
             new_path = path + [neighbor]
+
+            # add This neighbor to the queue with anew path starting with it's position
             q.put((neighbor, new_path))
             
-            # add this neighboring node to visited node so as it be checked
-            visited.add(neighbor)
-            
+            # asighn this node as visited 
+            visited.add(neighbor)    # which is a pair
+
+
+
+
+# The functions returns Neighbors of currenct node
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    # get the upper neighbor
+    if row > 0:
+        neighbors.append((row - 1, col))    # Up
+    
+    # get the down neighbor
+    if row + 1 < len(maze):
+        neighbors.append((row + 1, col))    # down
+
+    # get left node
+    if col > 0:
+        neighbors.append((row, col - 1))      # left
+
+    # get right node (neibore)
+    if col + 1 < len(maze[0]):
+        neighbors.append((row, col + 1))  # right
+    
+    
+    return neighbors
+
+
 def main(stdscr):
+    # colors
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    find_shortest_path(maze, stdscr)
+    find_path(maze, stdscr)
     stdscr.getch()
-
-
 wrapper(main)
